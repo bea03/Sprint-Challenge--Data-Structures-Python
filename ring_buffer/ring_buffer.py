cad40cffaffb7d75f90ffc7afec5dfcd03a1653d
Nicole Beamer <tulip@Nicoles-iMac.local>
2020-02-28 10:07:59 -0700

    working on buffer ring. not passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,5 +1,33 @@
 from doubly_linked_list import DoublyLinkedList
 
+'''Task 1. Implement a Ring Buffer Data Structure
+A ring buffer is a non-growable buffer with a fixed size. When the ring buffer is full and a new element is inserted, the oldest element in the ring buffer is overwritten with the newest element. This kind of data structure is very useful for use cases such as storing logs and history information, where you typically want to store information up until it reaches a certain age, after which you don't care about it anymore and don't mind seeing it overwritten by newer data.
+
+Implement this behavior in the RingBuffer class. RingBuffer has two methods, append and get. The append method adds elements to the buffer. The get method, which is provided, returns all of the elements in the buffer in a list in their given order. It should not return any None values in the list even if they are present in the ring buffer.
+
+You may not use a Python List in your implementation of the append method (except for the stretch goal)
+
+buffer = RingBuffer(3)
+
+buffer.get()   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+buffer.get()   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+buffer.get()   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+buffer.get()   # should return ['d', 'e', 'f']
+
+'''
 
 class RingBuffer:
     def __init__(self, capacity):
@@ -8,14 +36,25 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.capacity <= 0:
+            return
+        elif len(self.storage) == self.capacity:
+            if not self.current:
+                self.current = self.storage.tail
+            self.current.value = item
+            self.current = self.current.prev
+        else:
+            self.storage.add_to_head(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-
+        node = self.storage.head
+        while node:
+            list_buffer_contents.append(node.value)
+            node = node.next
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
